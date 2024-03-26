407920acab7f6b44c8fe7e7165142e73b0f0805d
Gwenell <01gwenell@proton.me>
2024-03-26 18:58:46 +0100

    3

--- pull.php
+++ pull.php
@@ -1,25 +1,27 @@
-<?php
-// Vérifiez si la requête est de type POST
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Exécutez git pull dans le répertoire spécifié
-    // Assurez-vous que l'utilisateur PHP a les permissions adéquates pour exécuter cette commande
-    // Important : protégez cette page avec une authentification
-    $output = shell_exec('git pull');
-    echo "<pre>$output</pre>";
-}
-?>
-
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Git Pull Button</title>
+    <?php include('head.php'); // Ensure you have a 'head.php' file with the necessary CSS links ?>
+    <title>Git Pull Page</title>
 </head>
-<body>
-    <!-- Le formulaire pointe sur lui-même pour la requête POST -->
-    <form action="" method="post">
-        <!-- Ajoutez une vérification d'authentification ici pour sécuriser cette action -->
-        <input type="submit" value="Pull from Git">
-    </form>
+<body class="bg-gray-200">
+    <div class="flex items-center justify-center h-screen">
+        <div class="w-full max-w-md">
+            <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <form action="pull.php" method="post">
+                    <input type="hidden" name="action" value="gitpull">
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">Pull from Git</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <?php
+    // Check if the form has been submitted
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_POST['action'] === 'gitpull') {
+        // Execute the git pull command and capture output
+        $output = shell_exec('cd /var/www/html/ServerManager && git pull 2>&1');
+        echo "<pre>$output</pre>";
+    }
+    ?>
 </body>
 </html>
