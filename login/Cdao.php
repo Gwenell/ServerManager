adbb7f75f803acb1727d63c06d1e626db8a6031c
Gwenell <01gwenell@proton.me>
2024-03-30 19:13:22 +0100

    sql

--- login/Cdao.php
+++ login/Cdao.php
@@ -23,13 +23,13 @@ class Cdao
     public function registerUser($username, $password, $email, $role, $status)
     {
         $lepdo = $this->getObjetPDO();
-        $sth = $lepdo->prepare("INSERT INTO Users (Username, Password, Email, Role, Status) VALUES (?, ?, ?, ?, ?)");
+        $sth = $lepdo->prepare("INSERT INTO users (Username, Password, Email, Role, Status) VALUES (?, ?, ?, ?, ?)");
         return $sth->execute([$username, $password, $email, $role, $status]);
     }
 
     public function loginUser($username, $password)
     {
-        $userInfo = $this->getTabDataFromSql("SELECT * FROM Users WHERE Username = ?", [$username]);
+        $userInfo = $this->getTabDataFromSql("SELECT * FROM users WHERE Username = ?", [$username]);
         if (!empty($userInfo) && password_verify($password, $userInfo[0]['Password'])) {
             // Après la connexion réussie, vous pouvez définir des variables de session ou effectuer d'autres opérations
             $_SESSION['user_id'] = $userInfo[0]['UserID']; // Exemple de définition de la variable de session
