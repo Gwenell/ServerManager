a02f6e50262391e3441ef26d86dabf377ebf4de3
Gwenell <01gwenell@proton.me>
2024-03-30 19:26:41 +0100

    1

--- gestion_server/pull.php
+++ gestion_server/pull.php
@@ -1,15 +1,9 @@
 <?php
 session_start();
 
-// Check if the request is a POST request
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Change '/path/to/your/repo' to the actual path of your Git repository
     $repositoryPath = '/var/www/html/ServerManager';
-
-    // Run the git pull command in the specified directory and capture the output
     $output = shell_exec('cd ' . escapeshellarg($repositoryPath) . ' && git pull 2>&1');
-    
-    // Display the output
     echo "<pre>$output</pre>";
 }
 ?>
